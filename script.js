bfba522a3be992bad84c2e60f5cbf573cc2cb2c3
Leonardo <106412608+leshyG@users.noreply.github.com>
2025-11-03 09:56:49 -0500

    Lógica modificada. El usuario ahora ingresada cadenas de texto. Ahora existe una función que puede convertir el texto a número para poder compararlo con la elección del oponente. Se trackean scores

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
-function obtenerEleccionComputadora() {
+function getComputerChoice() {
     return Math.floor(Math.random() * 3) + 1
 }
 
-function obtenerEleccionUsuario() {
-    return parseInt(prompt("¿Piedra (1), papel (2) o tijera (3)?"));
+function getUserChoice() {
+    let choice = prompt("¿Piedra, papel o tijera?");
+    if (choice === null || choice === undefined) {
+        console.error("Opción inválida!");
+        return -1;
+    }
+    return choice;
 }
 
-let userSelection = obtenerEleccionUsuario();
-let opponentSelection = obtenerEleccionComputadora();
-console.log(opponentSelection);
-if (userSelection === null || userSelection === undefined) {
-    console.log("Elija una opción válida!");
-} else if (userSelection === opponentSelection) {
-    console.log("Empate!")
-} else if (userSelection === 1) {
-    opponentSelection === 2 ? console.log("Perdiste!") : console.log("Ganaste!");
-} else if (userSelection === 2) {
-    opponentSelection === 3 ? console.log("Perdiste!") : console.log("Ganaste!");
-} else if (userSelection == 3) {
-    opponentSelection === 1 ? console.log("Perdiste!") : console.log("Ganaste!");
+function convertChoiceOpponent(choice) {
+    return (choice === 1) ? "Piedra" : ((choice === 2) ? "Papel" : "Tijera")
 }
+
+function convertChoiceUser(choice) {
+    choice = choice.toLowerCase();
+    return (choice === "piedra") ? 1 : ((choice === "papel") ? 2 : ((choice === "tijera") ? 3 : null));
+}
+
+function checkWinner(user, opp) {
+    if (user === opp) {
+        console.log("Empate!")
+    } else if (user === 1) {
+        if (opp === 2) {
+            console.log("Perdiste! Papel le gana a Piedra!")
+            return ++computerScore;
+        }
+        console.log("Ganaste!")
+        return ++humanScore;
+    } else if (user === 2) {
+        if (opp === 3) {
+            console.log("Perdiste! Tijera le gana a Papel!")
+        }
+        console.log("Ganaste!")
+        return ++humanScore;
+    } else if (user == 3) {
+        if (opp === 1) {
+            console.log("Perdiste! Piedra le gana a Tijera!")
+        }
+        console.log("Ganaste!")
+        return ++humanScore;
+    }
+}
+let humanScore = 0, computerScore = 0;
+let userSelection = convertChoiceUser(getUserChoice());
+let opponentSelection = getComputerChoice();
+
+console.log(convertChoiceOpponent(opponentSelection));
+console.log(userSelection);
+
+checkWinner(userSelection,opponentSelection)
+console.log(`El oponente va: ${computerScore}`)
+console.log(`El usuario va : ${humanScore}`)
