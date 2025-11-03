cb99170e58bae8343da6e8343e6116d9f679abe9
Leonardo <106412608+leshyG@users.noreply.github.com>
2025-11-03 10:41:35 -0500

    Se modifico la función converChoiceUser para poder establecer correctamente un error (antes no detenía el programa). Se añadió los returns faltantes a la función checkWinner. Se añadió más texto intuitivo y un bucle para jugar un N número de veces

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3) + 1
+    return Math.floor(Math.random() * 3) + 1 // 1; Piedra | 2: Papel | 3: Tijera
 }
 
 function getUserChoice() {
     let choice = prompt("¿Piedra, papel o tijera?");
-    if (choice === null || choice === undefined) {
-        console.error("Opción inválida!");
-        return -1;
+    if (choice === null) {
+        throw new Error("Operación cancelada!");
+    }
+    if (choice === "") {
+        throw new Error("No ingresaste una opción!");
     }
     return choice;
 }
@@ -16,8 +18,16 @@ function convertChoiceOpponent(choice) {
 }
 
 function convertChoiceUser(choice) {
-    choice = choice.toLowerCase();
-    return (choice === "piedra") ? 1 : ((choice === "papel") ? 2 : ((choice === "tijera") ? 3 : null));
+    choice = choice[0].toUpperCase() + choice.slice(1).toLowerCase();
+    if (choice === "Piedra") {
+        return 1;
+    } else if (choice === "Papel") {
+        return 2;
+    } else if (choice === "Tijera") {
+        return 3;
+    } else {
+        throw new Error("Opción inválida!!")
+    }
 }
 
 function checkWinner(user, opp) {
@@ -33,24 +43,38 @@ function checkWinner(user, opp) {
     } else if (user === 2) {
         if (opp === 3) {
             console.log("Perdiste! Tijera le gana a Papel!")
+            return ++computerScore;
         }
         console.log("Ganaste!")
         return ++humanScore;
     } else if (user == 3) {
         if (opp === 1) {
             console.log("Perdiste! Piedra le gana a Tijera!")
+            return ++computerScore;
         }
         console.log("Ganaste!")
         return ++humanScore;
     }
 }
+
+function playAround() {
+    let userText = getUserChoice();
+    let opponentSelection = getComputerChoice();
+
+    console.log(`El usuario eligió ${userText}`);
+    console.log(`El oponente eligió: ${opponentSelection}`);
+
+    userSelection = convertChoiceUser(userText);
+    checkWinner(userSelection, opponentSelection)
+    console.log(`El oponente va: ${computerScore}`)
+    console.log(`El usuario va : ${humanScore}`)
+}
+
 let humanScore = 0, computerScore = 0;
-let userSelection = convertChoiceUser(getUserChoice());
-let opponentSelection = getComputerChoice();
-
-console.log(convertChoiceOpponent(opponentSelection));
-console.log(userSelection);
-
-checkWinner(userSelection,opponentSelection)
-console.log(`El oponente va: ${computerScore}`)
-console.log(`El usuario va : ${humanScore}`)
+let playCount = parseInt(prompt("¿Cuántas partidas quieres jugar?"));
+if (isNaN(playCount) || playAround < 1) {
+    throw new Error("Asigne un valor válido!");
+}
+for (let i = 0; i < playCount; i++) {
+    playAround();
+}
